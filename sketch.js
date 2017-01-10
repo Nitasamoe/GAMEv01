3d55eb0b6729c39f4f0cea22e6b56b234977c00c
Tobias Schnider <schniderto@gmail.com>
2017-01-10 19:38:18 +0100

    v02

--- sketch.js
+++ sketch.js
@@ -1,28 +1,69 @@
 // "strict mode"
 //INPUT=========================================================================
 var worldSetup = [
+  [false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
   [false,true ,true ,true ,true ,false,true ,false,true ,true ,false,false,false,false,false,true ,true  ],
   [false,false,true ,true ,true ,true ,true ,true ,true ,true ,true ,true ,false,false,false,true ,true  ],
   [false,true ,true ,false,true ,true ,false,true ,true ,true ,true ,true ,true ,true ,true ,true ,false ],
   [false,true ,true ,true ,true ,true ,true ,true ,true ,true ,true ,false,false,true ,false,false,false ],
   [false,false,false,false,false,true ,false,false,true ,true ,false,false,false,true ,false,false,false ],
   [false,false,false,false,false,false,false,false,false,true ,true ,true ,true ,true ,true ,true ,false ],
-  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ]
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
+  [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false ],
 ]
-var sizeBox = 50;
-
-var marioInput = {
-  speedValue  : 1,
-  jumpValue   : 30,
-  sizeValue   : 40
+var sizeBox     = 35,
+    marioInput  = {
+        speedValue  : 1,
+        jumpValue   : 22,
+        sizeValue   : 30
+    }
+var garbArray = [];
+var timeSpeed = 100;
+var worldClock = {
+    day : 0,
+    hou : 0,
+    min : 0,
+    sec :0
 }
-
-
+var gameover = false;
+var blockHeight = 0;
+var worldHeight;
+var worldWidth;
+var lastPos={
+  x:0,
+  y:0
+}
 //============================END OF INPUT=======================================
 //===============================================================================
 function setup() {//=========Start SETUP=========================================
   createCanvas(worldSetup[0].length*sizeBox, worldSetup.length*sizeBox)
   frameRate(60);
+  worldHeight = worldSetup.length*sizeBox;
+  worldWidth = worldSetup[0].length*sizeBox;
+// Setup Game Clock
+  setInterval(function(){
+    worldClock.sec += 1;
+    blockHeight += 0.5;
+    if(worldClock.sec === 60){worldClock.sec = 0;worldClock.min += 1
+                                garbArray.push(new Garbage(lastPos.x,lastPos.y));
+                              }
+    if(worldClock.min === 60){worldClock.min = 0;worldClock.hou += 1}
+    if(worldClock.hou === 60){worldClock.hou = 0;worldClock.day += 1}
+    if(worldClock.sec%40===0){lastPos.x = mario.pos.x;lastPos.y = mario.pos.y;}
+
+  },timeSpeed);
 // Setting up Box constructor
   function Box(x,y){
     this.top = y*sizeBox;
@@ -52,6 +93,20 @@ function setup() {//=========Start SETUP========================================
     this.colPoints; // arr of the four points of the hit box
     // method to establish the four Points of hitBox
     this.collisionPointsSetup = function(){this.colPoints = createCollisionPoints(this.pos,this.size);};
+    this.amIdead = amIdead;
+  }
+// Establish Garbage constructor
+  function Garbage(x,y){
+    this.pos    = createVector(x,y);
+    this.size   = 10;
+    this.vel    = createVector(0,0);
+    this.acc    = createVector(0,0);
+    this.display= function(){ ellipse(this.pos.x, this.pos.y, this.size) };
+    this.collide= function(){var colP = this.colPoints; collision(boxArr, colP)};
+    this.colPoints; // arr of the four points of the hit box
+    // method to establish the four Points of hitBox
+    this.collisionPointsSetup = function(){this.colPoints = createCollisionPoints(this.pos,this.size);};
+    this.exist = true;
   }
   mario = new Mario() // create Mario from the constructor
 }//=========END SETUP==========================================================
@@ -61,6 +116,8 @@ function draw(){//=========Start DRAW==========================================
   gravity = createVector(0,1);
 //WORLD BUILDING---------------------------------------------------------
    worldBuilding();
+   blockRender();
+   renderGarbage();
 //MARIO BUILDING-----------------------------------------------
   mario.vel.mult(0); // throw velocity bck to zero
   //Move Mario------------------------------------------------------
@@ -71,19 +128,26 @@ function draw(){//=========Start DRAW==========================================
 // Question Collision-------------------------------------------------
   mario.collisionPointsSetup(); // establish the hit box through four points of mario
   mario.collide(); // check if there is a collision and correct the posiiton
+  mario.amIdead();
 // RENDER MARIO-----------------------------------------------------
   mario.display();
+  blockRender();
 //change acceleration based on the pressed key
-  if(keyIsPressed===true){
+  if(keyIsPressed===true && gameover === false){
     if(keyCode === RIGHT_ARROW){
       mario.acc.add(createVector(marioInput.speedValue,0));
     }
-    if(keyCode === LEFT_ARROW){
+    if(keyCode === LEFT_ARROW && gameover === false){
       mario.acc.add(createVector(-marioInput.speedValue,0))
     }
   }
 } // Ende draw();==============================================================
 //=============================================================================
+function blockRender(){
+  // RENDER BLOCK THAT RISES=====================================================
+  fill(180,20,20,100);
+  quad(0 , worldHeight-blockHeight , worldWidth , worldHeight-blockHeight , worldWidth , worldHeight , 0 , worldHeight);
+}
 // Establish the function for creating the four points of our HitBox
 function createCollisionPoints(pos,size){
     var arr = [];
@@ -140,7 +204,7 @@ function keyReleased(){
 }
 function keyPressed(){
   //Control jump
-      if(keyCode === UP_ARROW && mario.inAir === false){
+      if(keyCode === UP_ARROW && mario.inAir === false && gameover === false){
         mario.acc.add(createVector(0,-marioInput.jumpValue));
         mario.inAir = true;
       }
@@ -154,10 +218,25 @@ function worldBuilding(){
       if(worldSetup[y][x]===false){
         // Start Punkt links Oben
         (function(){
-            fill(0,0,0);
+            noStroke();
+            fill(0,0,0,10);
             rect(x*sizeBox,y*sizeBox,sizeBox,sizeBox);
             })()
       }
     }
+  }
+}
+function amIdead(){
+  if(mario.pos.y > worldHeight-blockHeight){
+    textSize(80);
+    fill(0,0,0)
+    text("Game Over", mario.pos.x-250, mario.pos.y);
+    gameover = true;
+  }
+}
+function renderGarbage(){
+  for(var i = 0 ; i<garbArray.length ; i++){
+      garbArray[i].display();
+      if(p5.Vector.dist(mario.pos, garbArray[i].pos)<10){garbArray.splice(i,1);blockHeight-=10}
   }
 }
